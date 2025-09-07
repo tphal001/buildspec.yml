e56e721e98f480160b6c094298ad9b88e2053d40
Tushar Phalke <phalke.tushar1@gmail.com>
2025-09-07 09:51:34 -0500

    Add files via upload

---
+++ welcome.java
@@ -0,0 +1,10 @@
+package testMavenPro;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Tushar!");
+	}
+
+}
